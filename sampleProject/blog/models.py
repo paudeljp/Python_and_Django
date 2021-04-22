78599019def2b484c2b6c81d6e79033c42658f64
Jeevan Paudel <jeevanpaudel77@gmail.com>
2021-04-22 16:45:47 +0545

    blog migration

--- sampleProject/blog/models.py
+++ sampleProject/blog/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- sampleProject/sampleProject/settings.py
+++ sampleProject/sampleProject/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BUILT_IN_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+THIRD_PARTY_APPS = []
+USER_DEFINED_APPS = ['blog']
+INSTALLED_APPS = BUILT_IN_APPS + THIRD_PARTY_APPS + USER_DEFINED_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
